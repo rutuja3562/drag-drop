aa508c82cb27b09ba312e2e3ae434f43f658faba
Rutuja Patil <rspatil3562@gmail.com>
2022-10-17 15:53:12 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div className="container">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
